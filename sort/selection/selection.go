a12e6c406ab9905182c80c01697e4642e6f62bec
zasuchilas <zasuchilas@ya.ru>
2025-09-19 11:03:15 +0300

    added selection sort

---
+++ sort/selection/selection.go
@@ -0,0 +1,26 @@
+package selection
+
+func Sort(arr []int) []int {
+	newArr := make([]int, len(arr))
+	for i := range arr {
+		idx := findSmallest(arr)
+		newArr[i] = arr[idx]
+
+		// removing the added element from the source array
+		arr = append(arr[:idx], arr[idx+1:]...) // arr = slices.Delete(arr, idx, idx+1)
+		// TODO: it turns out that we are spoiling the incoming array
+	}
+	return newArr
+}
+
+func findSmallest(arr []int) int {
+	smallest := arr[0]
+	smallestIndex := 0
+	for i := range arr {
+		if arr[i] < smallest {
+			smallest = arr[i]
+			smallestIndex = i
+		}
+	}
+	return smallestIndex
+}
---
+++ sort/selection/selection_test.go
@@ -0,0 +1,72 @@
+package selection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "success",
+			args: args{
+				arr: []int{5, 3, 1, 7, 9},
+			},
+			want: []int{1, 3, 5, 7, 9},
+		},
+		{
+			name: "success",
+			args: args{
+				arr: []int{5},
+			},
+			want: []int{5},
+		},
+		{
+			name: "success",
+			args: args{
+				arr: []int{5, -3, 0, 12, 4, -6, 0, 10},
+			},
+			want: []int{-6, -3, 0, 0, 4, 5, 10, 12},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findSmallest(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "success",
+			args: args{
+				arr: []int{5, 3, 1, 7, 9},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findSmallest(tt.args.arr); got != tt.want {
+				t.Errorf("findSmallest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
